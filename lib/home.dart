060eadfd891f1589f8ee48fb8676b4ae9defa0cf
Bwarlock <110149913+Bwarlock@users.noreply.github.com>
2022-10-21 17:37:19 +0530

    added side drawer

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dealware/main.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -12,6 +13,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<String> store = [
+    "2game",
+    "AllYouPlay",
+    "Amazon",
+    "Blizzard",
+    "Direct2Drive",
+    "DLGamer",
+    "Dreamgame",
+    "Epic Game Store",
+    "eTail.Market",
+    "Fanatical",
+    "FireFlower",
+    "Game Jolt",
+    "GameBillet",
+    "GamersGate",
+    "Gamerload",
+    "GamesPlanet DE",
+    "GamesPlanet FR",
+    "GamesPlanet UK",
+    "GamesPlanet US",
+    "GamesRepublic",
+    "GameStop PC",
+    "GOG",
+    "GreenManGaming",
+    "Humble Store",
+    "Humble Widgets",
+    "IndieGala Store",
+    "Itch.io",
+    "JoyBuggy",
+    "MacGamesStore",
+    "Microsoft Store",
+    "Newegg",
+    "Noctre",
+    "Nuuvem",
+    "Origin",
+    "Square Enix",
+    "Steam",
+    "Ubisoft Store",
+    "Voidu",
+    "WinGameStore"
+  ];
+  final List<String> gameType = [
+    "DLC",
+    "Package",
+    "Early Access",
+    "Trading Cards",
+    "Achievements"
+  ];
+
+  Widget getTile(String str) {
+    return ListTile(
+      contentPadding: const EdgeInsets.fromLTRB(15, 0, 4, 0),
+      leading: Text(
+        str,
+        style: const TextStyle(
+          fontSize: 18,
+        ),
+      ),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (isChanged) {},
+      ),
+      onTap: () {},
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +89,198 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text(
+                  "Settings",
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text(
+                  "Free Games",
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+                trailing: Checkbox(
+                  value: false,
+                  onChanged: (isChanged) {},
+                ),
+                onTap: () {},
+              ),
+            ),
+            ListTile(
+              leading: const Text(
+                "Filters",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ExpansionTile(
+              title: const Text(
+                "Store",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              children: store.map((str) => getTile(str)).toList(),
+            ),
+            ExpansionTile(
+              title: const Text(
+                "Price",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    SizedBox(
+                      width: 80,
+                      height: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "min",
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Text("to"),
+                    SizedBox(
+                      width: 80,
+                      height: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "max",
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.add_box),
+                  ],
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: const Text(
+                "Price Cut %",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    SizedBox(
+                      width: 80,
+                      height: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "0",
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Text("to"),
+                    SizedBox(
+                      width: 80,
+                      height: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "100",
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.add_box),
+                  ],
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: const Text(
+                "Game Type",
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              children: gameType.map((str) => getTile(str)).toList(),
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const SizedBox(
+              width: 80,
+              height: 50,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "min",
+                  filled: true,
+                ),
+              ),
+            ),
             const Text("Hiiiii"),
             Container(
               width: 200,
               height: 200,
               decoration: BoxDecoration(
                   border: Border.all(
-                    color: Colors.red,
-                    width: 2,
-                  )
-              ),
+                color: Colors.red,
+                width: 2,
+              )),
               child: Image.asset("assets/848450.jpg"),
+            ),
+            const Text(
+              'Choose your theme:',
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                /// //////////////////////////////////////////////////////
+                /// Change theme & rebuild to show it using these buttons
+                ElevatedButton(
+                    onPressed: () =>
+                        MyApp.of(context).changeTheme(ThemeMode.light),
+                    child: const Text('Light')),
+                ElevatedButton(
+                    onPressed: () =>
+                        MyApp.of(context).changeTheme(ThemeMode.dark),
+                    child: const Text('Dark')),
+                ElevatedButton(
+                    onPressed: () =>
+                        MyApp.of(context).changeTheme(ThemeMode.system),
+                    child: const Text('System')),
+
+                /// //////////////////////////////////////////////////////
+              ],
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,34 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
 }
 
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  ThemeMode _themeMode = ThemeMode.system;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
       theme: ThemeData(primarySwatch: Colors.blue,),
       darkTheme: ThemeData.dark(), // standard dark theme
-      themeMode: ThemeMode.system, // device controls theme
+      themeMode: _themeMode, // device controls theme
       home: const MyHomePage(title: 'Dealware'),
     );
   }
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
+
+  // MyApp.of(context).changeTheme(ThemeMode.light)
+  // MyApp.of(context).changeTheme(ThemeMode.dark)
 }
 
 
